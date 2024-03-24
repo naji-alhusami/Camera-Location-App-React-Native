b7069db9bef3b10c5e0fe9a07c80043b89736835
naji-alhusami <najihussami@gmail.com>
2024-03-24 13:44:18 +0100

    passed places to AllPlaces page

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,8 +1,14 @@
 import { StyleSheet } from "react-native";
 import PlaceForm from "../components/Places/PlaceForm";
 
-function AddPlace() {
-  return <PlaceForm />;
+function AddPlace({ navigation }) {
+  function createPlaceHandler(place) {
+    navigation.navigate("AllPlaces", {
+      place: place,
+    });
+  }
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
 }
 
 export default AddPlace;
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,8 +1,19 @@
 import { StyleSheet } from "react-native";
 import PlacesList from "../components/Places/PlacesList";
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 
-function AllPlaces() {
-  return <PlacesList />;
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]); // place from AddPlace Screen
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
